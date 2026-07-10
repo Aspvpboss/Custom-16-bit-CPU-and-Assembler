745c3feeb37795c592bf0460beea9b20db0dc41e
Ben <b.f.vaughan2026@gmail.com>
2026-07-10 09:35:07 -0400

    write_memory function is now hypothetically functional, although I haven't truely tested it yet

--- Emulator/src/pipeline/execute_memory.c
+++ Emulator/src/pipeline/execute_memory.c
@@ -24,34 +24,36 @@ int write_memory(EMU_Ram *ram, u16 address, u16 value, bool sixteen_bit_write){
 
     u8 *active_vram = ram->vram_selector ? ram->vram_two : ram->vram_one;
 
-    if(sixteen_bit_write == true){
 
-        if(address + 1 > 0xffff) return 1;
+    if(address > 0x6EFF && address < 0x7F00){
 
-        if(address > 0x6EFF && address < 0x7F00){
+        if(sixteen_bit_write == true && address + 1 > 0x7EFF) return 1;
 
-            active_vram[address] = (value & 0x00ff);
-            active_vram[address + 1] = (value & 0xff00) >> 8;
+        u16 offset = address - 0x6F00; 
 
-        } else{
+        active_vram[offset] = (value & 0x00ff);
+        active_vram[offset + 1] = sixteen_bit_write ? (value & 0xff00) >> 8 : active_vram[offset + 1];
 
-            ram->ram[address] = value & 0x00ff;
-            ram->ram[address + 1] = (value & 0xff00) >> 8;
+    } else if(address > 0x7EFF && address < 0x8000){
+     
+        if(sixteen_bit_write == true && address + 1 > 0x7FFF) return 1;
 
-        }
+        u16 offset = address - 0x7F00;
 
+        ram->mmio[offset] = value & 0x00ff;
+        ram->mmio[offset + 1] = sixteen_bit_write ? (value & 0xff00) >> 8 : ram->mmio[offset + 1];
 
-    } else if (sixteen_bit_write == false){
+    } else{
 
-        if(address > 0x6EFF && address < 0x7F00){
+        if(sixteen_bit_write == true && address + 1 > 0x6EFF) return 1;
 
-            ram->ram[address] = (value & 0x00ff);
+        u16 offset = address - 0x0100;
 
-        }
-
-        ram->ram[address] = (value & 0x00ff) >> 8;
-
+        ram->ram[offset] = value & 0x00ff;
+        ram->ram[offset + 1] = sixteen_bit_write ? (value & 0xff00) >> 8 : ram->ram[offset + 1];
+        
     }
+
 
     return 0;
 }
