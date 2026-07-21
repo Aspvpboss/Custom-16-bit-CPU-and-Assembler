1dd4039e6bce5de836430b98aeecf4d3d4c87cae
Ben Vaughan <b.f.vaughan2026@gmail.com>
2026-07-21 10:56:16 -0400

    i am implemented string tokenizing, it wasn't too bad

--- Assembler/src/tokenize.rs
+++ Assembler/src/tokenize.rs
@@ -24,6 +24,11 @@ pub struct RawTokens{
 }
 
 
+enum Mode{
+    Token,
+    InString,
+}
+
 
 fn get_state(ch: char) -> State {
 
@@ -96,62 +101,87 @@ pub fn tokenize(raw_file: Vec<RawFileLines>) -> Vec<RawTokens>{
         let mut current_state = State::Whitespace;
         let mut prev_state = State::Whitespace;
         let mut current_column: u32 = 0;
+        let mut mode = Mode::Token;
 
         for (byte_index, ch) in raw_line.string.char_indices() {
             current_state = get_state(ch);
             current_column += 1;
 
-            if !matches!(current_state, State::Whitespace) && matches!(prev_state, State::Whitespace) {
-                token_byte_index = byte_index;
-            }
-
-            // Handle comment
-            if matches!(current_state, State::Slash) && matches!(prev_state, State::Slash){
-                break;
-            }
-
-            // Handle ':' and '::'
-            if matches!(current_state, State::Colon) {
-                
-                if matches!(prev_state, State::Character) {
+            if matches!(mode, Mode::Token){
+                if matches!(prev_state, State::Quote){
                     flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column);
+                    token_byte_index = byte_index;
+                    mode = Mode::InString;
+                }
+            } else {
+                if matches!(prev_state, State::Quote){
+                    mode = Mode::Token;
+                }
+            }
+
+            match mode{
+                Mode::Token => {
+                    if !matches!(current_state, State::Whitespace) && matches!(prev_state, State::Whitespace) {
+                        token_byte_index = byte_index;
+                    }
+
+                    // Handle comment
+                    if matches!(current_state, State::Slash) && matches!(prev_state, State::Slash){
+                        break;
+                    }
+
+                    // Handle ':' and '::'
+                    if matches!(current_state, State::Colon) {
+                        
+                        if matches!(prev_state, State::Character) {
+                            flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column);
+                        }
+
+                        if matches!(prev_state, State::Colon) {
+                            tokens.pop();
+                            token_byte_index = byte_index + 1;
+                            push_token(&raw_line, "::", &mut tokens, current_column);
+                            prev_state = State::Whitespace;
+                            continue;
+                        }
+
+                        // Single ':'
+                        push_token(&raw_line, ":", &mut tokens, current_column);
+
+                        prev_state = State::Colon;
+                        token_byte_index = byte_index + 1;
+                        continue;
+                    }
+
+                    // handles single tokens ('{', '}', '[', ']')
+                    if match_single_chars(&prev_state) {
+                        flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column);
+                    } 
+                    // handles full tokens and consective single tokens 
+                    if (match_single_chars(&current_state) || matches!(current_state, State::Whitespace)) && matches!(prev_state, State::Character){
+                        flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column - 1);
+                    }
+                        
+                    prev_state = current_state;
                 }
 
-                if matches!(prev_state, State::Colon) {
-                    tokens.pop();
-                    token_byte_index = byte_index + 1;
-                    push_token(&raw_line, "::", &mut tokens, current_column);
-                    prev_state = State::Whitespace;
-                    continue;
+                Mode::InString => {
+                    if matches!(current_state, State::Quote) {
+                        flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column);
+                    }
+                    
+                    prev_state = current_state;
                 }
-
-                // Single ':'
-                push_token(&raw_line, ":", &mut tokens, current_column);
-
-                prev_state = State::Colon;
-                token_byte_index = byte_index + 1;
-                continue;
-            }
-
-            // handles single tokens ('{', '}', '[', ']')
-            if match_single_chars(&prev_state) {
-                flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column);
-            } 
-            // handles full tokens and consective single tokens 
-            if (match_single_chars(&current_state) || matches!(current_state, State::Whitespace)) && matches!(prev_state, State::Character){
-                flush_token(&raw_line, &mut tokens, &mut token_byte_index, byte_index, current_column - 1);
-            }
-                
-                prev_state = current_state;
-            }
-
-            if !matches!(prev_state, State::Whitespace) && !(matches!(current_state, State::Slash) && matches!(prev_state, State::Slash)){
-                let slice = raw_line.string[token_byte_index..].to_string();
-                if !slice.is_empty() {
-                    tokens.push(RawTokens {raw_token: slice, line: raw_line.line_number, column: current_column});
-                } 
             }
         }
+
+        if !matches!(prev_state, State::Whitespace) && !(matches!(current_state, State::Slash) && matches!(prev_state, State::Slash)){
+            let slice = raw_line.string[token_byte_index..].to_string();
+            if !slice.is_empty() {
+                tokens.push(RawTokens {raw_token: slice, line: raw_line.line_number, column: current_column});
+            } 
+        }
+    }
 
     tokens
 }
