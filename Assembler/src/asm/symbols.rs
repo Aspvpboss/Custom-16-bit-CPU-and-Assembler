212d23aeb3e8e35fb1ff539488cb4c29c75901be
Ben Vaughan <b.f.vaughan2026@gmail.com>
2026-08-02 10:07:28 -0400

    fix: methods in symbols should be good now

--- Assembler/src/asm/symbols.rs
+++ Assembler/src/asm/symbols.rs
@@ -30,13 +30,13 @@ pub enum SymbolType{
 
 
 pub struct Symbol {
-    symbol: SymbolType,
-    name : String,
-    is_public: bool,
+    pub symbol: SymbolType,
+    pub name : String,
+    pub is_public: bool,
 }
 
 impl Symbol {
-    fn new(name : String, symbol_type : SymbolType, is_public : bool) -> Rc<Symbol> {
+    pub fn new(name : String, symbol_type : SymbolType, is_public : bool) -> Rc<Symbol> {
         Rc::new(Symbol {symbol: symbol_type, name: name.clone(), is_public: is_public})
     }
 }
@@ -47,11 +47,30 @@ pub struct SymbolTable{
 }
 
 impl SymbolTable {
-    fn new() -> SymbolTable {
+
+    fn insert_builtin_types(&mut self, file_name : &String){
+        
+        let byte_t = Symbol::new(".byte".to_string(), SymbolType::VarType(VarType::default()), false);
+        let word_t = Symbol::new(".word".to_string(), SymbolType::VarType(VarType::default()), false);
+        let string_t = Symbol::new(".string".to_string(), SymbolType::VarType(VarType::default()), false);
+        
+        let entry = self.symbol_table.entry(file_name.clone()).or_default();
+        entry.push(byte_t);
+        entry.push(word_t);
+        entry.push(string_t);
+    }
+
+    pub fn new() -> SymbolTable {
         SymbolTable { symbol_table: HashMap::new() }
     }
 
-    fn add_symbol(&mut self, file_name : String, symbol : &Rc<Symbol>) -> Option<AsmError> {
+    pub fn add_symbol(&mut self, file_name : String, symbol : &Rc<Symbol>) -> Option<AsmError> {
+
+        if !self.symbol_table.contains_key(&file_name) {
+            self.symbol_table.insert(file_name.clone(), Vec::new());
+            self.insert_builtin_types(&file_name);
+        }
+
         if let Some(existing_symbols) = self.symbol_table.get(&file_name) {
             for existing in existing_symbols {
                 if existing.name == symbol.name {
@@ -68,10 +87,15 @@ impl SymbolTable {
         None
     }
 
-    fn include_file_symbols(&mut self, source_file: &String, dest_file: String) -> Option<Vec<AsmError>>{
+    pub fn include_file_symbols(&mut self, source_file: &String, dest_file: String) -> Option<Vec<AsmError>>{
 
         let mut errors: Vec<AsmError> = Vec::new();
-        
+        if source_file == &dest_file {
+            errors.push(AsmError::new(
+                format!("File {} cannot include itself", dest_file),
+                AsmErrorType::Include));
+            return Some(errors);
+        }
         let Some(source_table) = self.symbol_table.get(source_file) else{
             errors.push(AsmError::new(
                 format!("Tried to included symbols from {} that doesn't exist", source_file), 
@@ -82,6 +106,10 @@ impl SymbolTable {
         let symbols = source_table.clone();
         
         for symbol in &symbols {
+            if symbol.is_public == false {
+                continue;
+            }
+
             if let Some(error) = self.add_symbol(dest_file.clone(), symbol){
                 errors.push(error);
             }
