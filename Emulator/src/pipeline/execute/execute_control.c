869427139889acf3db9062078fbf52ab8cffe85f
Ben Vaughan <b.f.vaughan2026@gmail.com>
2026-09-01 16:52:50 -0400

    feat: added control flow instructions into new execute_control.c

---
+++ Emulator/src/pipeline/execute/execute_control.c
@@ -0,0 +1,31 @@
+#include "pipeline/execute/execute_control.h"
+
+int exe_syscall(Emulator *emu, EMU_Decoded_Instruction *instruction){
+    // nothing here for now
+    return 0;
+}
+
+int exe_jmp(Emulator *emu, EMU_Decoded_Instruction *instruction){
+
+    return 0;
+}
+
+int exe_jif(Emulator *emu, EMU_Decoded_Instruction *instruction){
+
+    return 0;
+}
+
+int exe_cal(Emulator *emu, EMU_Decoded_Instruction *instruction){
+
+    return 0;
+}
+
+int exe_cif(Emulator *emu, EMU_Decoded_Instruction *instruction){
+
+    return 0;
+}
+
+int exe_ret(Emulator *emu, EMU_Decoded_Instruction *instruction){
+
+    return 0;
+}
---
+++ Emulator/src/pipeline/execute/execute_control.h
@@ -0,0 +1,8 @@
+#ifndef EXECUTE_MATH_H__
+#define EXECUTE_MATH_H__
+
+#include "hardware/emulator.h"
+#include "pipeline/instruction.h"
+
+
+#endif
